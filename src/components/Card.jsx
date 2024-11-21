d3d35fc4a6bcf4ae556fa2796792206e70466147
rohitverse <rohitkr23072001@gmail.com>
2024-11-21 16:43:58 +0530

    dragElastic whileDrag  commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ import { motion } from "framer-motion"
 const Card = ({ data,reference }) => {
   return (
     <>
-      <motion.div drag dragConstraints={reference} className=" flrx-shrink-0 relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white py-10 px-8 overflow-hidden ">
+      <motion.div drag dragConstraints={reference} whileDrag={{scale:1.2}} dragElastic={.1} className=" flrx-shrink-0 relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white py-10 px-8 overflow-hidden ">
         <FaRegFileAlt />
         <p className="text-sm m-5 font-semibold leading-tight">{data.desc}</p>
         <div className="footer absolute bottom-0  w-full left-0  ">
